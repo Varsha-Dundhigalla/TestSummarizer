8d4c2a0ee5d5cb3c9d88c2292462f4de10c732e0
Varsha-Dundhigalla <varshasfd@gmail.com>
2024-03-19 21:04:30 -0400

    added data validation

---
+++ src/textSummarizer/pipeline/data_validation_stage2.py
@@ -0,0 +1,24 @@
+from textSummarizer.config.configuration import ConfigurationManager
+from textSummarizer.components.data_validation import DataValiadtion
+from textSummarizer.logging import logger
+
+
+class DataValidationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValiadtion(config=data_validation_config)
+        data_validation.validate_all_files_exist()
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_validation = DataValidationTrainingPipeline()
+   data_validation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
